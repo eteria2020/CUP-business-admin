70d956d8ff314c887d5f5d31db13ed971f790cf2
Andrea Cadorin <cadorin89@gmail.com>
2016-05-06 14:10:36 +0200

    Time packages must be assigned as buyable to businesses

--- module/Application/src/Application/Controller/TimePackagesController.php
+++ module/Application/src/Application/Controller/TimePackagesController.php
@@ -12,10 +12,6 @@ use ZfcUser\Exception\AuthenticationEventException;
 class TimePackagesController extends AbstractActionController
 {
     /**
-     * @var AuthenticationService
-     */
-    private $authService;
-    /**
      * @var BusinessTimePackageService
      */
     private $businessTimePackageService;
@@ -23,27 +19,23 @@ class TimePackagesController extends AbstractActionController
     /**
      * EmployeesController constructor.
      * @param BusinessTimePackageService $businessTimePackageService
-     * @param AuthenticationService $authService
      */
-    public function __construct(
-        BusinessTimePackageService $businessTimePackageService,
-        AuthenticationService $authService
-    ) {
+    public function __construct(BusinessTimePackageService $businessTimePackageService)
+    {
         $this->businessTimePackageService = $businessTimePackageService;
-        $this->authService = $authService;
     }
 
     public function timePackagesAction()
     {
         return new ViewModel([
-            'business' => $this->retrieveAuthenticatedUser()->getBusiness()
+            'business' => $this->identity()->getBusiness()
         ]);
     }
     public function buyAction()
     {
+        $business = $this->identity()->getBusiness();
         $timePackageId = $this->params()->fromRoute('id', 0);
         if ($timePackageId != 0) {
-            $business = $this->retrieveAuthenticatedUser()->getBusiness();
             $this->businessTimePackageService->buyTimePackage($business, $timePackageId);
             $this->flashMessenger()->addSuccessMessage($this->translatorPlugin()->translate("Pacchetto minuti acquistato con sucesso"));
 
@@ -51,21 +43,8 @@ class TimePackagesController extends AbstractActionController
         }
 
         return new ViewModel([
-            'business' => $this->retrieveAuthenticatedUser()->getBusiness(),
-            'packages' => $this->businessTimePackageService->getBuyablePackages()
+            'business' => $business,
+            'packages' => $this->businessTimePackageService->findBuyablePackages($business)
         ]);
-    }
-
-    /**
-     * @return Webuser
-     */
-    private function retrieveAuthenticatedUser()
-    {
-        $user = $this->authService->getIdentity();
-        if ($user instanceof Webuser) {
-            return $user;
-        } else {
-            throw new AuthenticationEventException($this->translatorPlugin()->translate("Errore di autenticazione"));
-        }
     }
 }
