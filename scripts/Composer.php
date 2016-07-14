b695326439010997f61757795ab46f70b33a2c38
Marco Perone <m.perone@mvlabs.it>
2016-07-14 09:23:29 +0200

    composer script to retrieve dependencies

---
+++ scripts/Composer.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Scripts;
+
+use Composer\Installer\InstallerEvent;
+use Composer\Json\JsonFile;
+use Composer\Semver\VersionParser;
+use Composer\Repository\RepositoryFactory;
+use Composer\Factory;
+use Composer\Script\Event;
+
+class Composer
+{
+    public static function addSubreposDependencies(InstallerEvent $event)
+    {
+        $autoloadDev = $event->getComposer()->getPackage()->getDevAutoload();
+
+        if (isset($autoloadDev['psr-4'])) {
+            $autoloadDevPackages = $autoloadDev['psr-4'];
+
+            foreach ($autoloadDevPackages as $name => $folder) {
+                $composerJsonPath = dirname($folder) . '/composer.json';
+
+                $jsonFile = new JsonFile($composerJsonPath);
+                $composerJson = $jsonFile->read();
+
+                if (isset($composerJson['require'])) {
+                    $versionsParser = new VersionParser();
+
+                    foreach ($composerJson['require'] as $package => $version) {
+                        $constraint = $versionsParser->parseConstraints($version);
+                        $event->getRequest()->install($package, $constraint);
+                    }
+                }
+            }
+        }
+    }
+
+    public static function addSubreposRepositories(Event $event)
+    {
+        $autoloadDev = $event->getComposer()->getPackage()->getDevAutoload();
+
+        if (isset($autoloadDev['psr-4'])) {
+            $autoloadDevPackages = $autoloadDev['psr-4'];
+
+            foreach ($autoloadDevPackages as $name => $folder) {
+                $composerJsonPath = dirname($folder) . '/composer.json';
+
+                $jsonFile = new JsonFile($composerJsonPath);
+                $composerJson = $jsonFile->read();
+
+                if (isset($composerJson['repositories'])) {
+                    $composer = $event->getComposer();
+                    $repositoryManager = $composer->getRepositoryManager();
+                    $io = $event->getIo();
+                    $config = Factory::createConfig($io);
+
+                    foreach ($composerJson['repositories'] as $repositoryData) {
+                        $repository = RepositoryFactory::createRepo(
+                            $io,
+                            $config,
+                            $repositoryData
+                        );
+
+                        $repositoryManager->addRepository($repository);
+                    }
+                }
+            }
+        }
+    }
+}
